d2565ae2785dd4eeee8b4b9be7a200b44384bdd7
Alexey Stankevich <alexey.stn@gmail.com>
2019-06-27 22:54:53 +0300

    Optimized callback

--- memo_game/memo.py
+++ memo_game/memo.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
-import time
 import random
-import colorsys
 
 pad = 10
-size_x = 120
-size_y = 80
+size_x = 100
+size_y = 75
 
-num_x = 4
-num_y = 6
+num_x = 5
+num_y = 10
 
 num_img = num_x * num_y // 2
 
@@ -19,6 +17,9 @@ card_status = 0
 
 card_clicked = 0
 
+clicked_point = 0
+
+clicked_point = 0
 
 def generate_field(card_status, combination):
     global num_x, num_y, size_x, size_y, pad
@@ -31,7 +32,8 @@ def generate_field(card_status, combination):
                 continue
             if card_status[y, x] == 2:
                 filename = './img/{0:02d}.jpg'.format(combination[y, x])
-                im = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+                print(filename)
+                im = cv2.imread(filename)
                 im = cv2.resize(im, (size_x, size_y), cv2.INTER_CUBIC)
             else:
                 im = 200
@@ -40,18 +42,11 @@ def generate_field(card_status, combination):
 
 
 def click(event, x, y, flags, param):
-    global size_x, size_y, pad
-    global card_status
-    global card_clicked
+    global clicked_point
     if event == cv2.EVENT_LBUTTONDOWN:
-        x_int = x // (size_x + pad)
-        x_frac = x % (size_x + pad)
-        y_int = y // (size_y + pad)
-        y_frac = y % (size_y + pad)
-        if (x_frac > pad) & (y_frac > pad):
-            if card_status[y_int, x_int] == 1:
-                card_clicked = (y_int, x_int)
-    return
+        clicked_point = y, x
+
+
             
 counter = 1
 
@@ -72,37 +67,44 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break;
-    if card_clicked: 
-        if opened_cards_number == 0:
-            opened_cards_number = 1
-            x0 = card_clicked[1]
-            y0 = card_clicked[0]
-            card_status[y0, x0] = 2
-            image = generate_field(card_status, combination)
-            cv2.imshow(window_name, image)
-        elif opened_cards_number == 1:
-            opened_cards_number = 2
-            x1 = card_clicked[1]
-            y1 = card_clicked[0]
-            card_status[y1, x1] = 2
-            image = generate_field(card_status, combination)
-            cv2.imshow(window_name, image)
-            cv2.waitKey(1000)
-            counter += 1
-            if combination[y0, x0] == combination[y1, x1]:
-                card_status[y0, x0] = 0
-                card_status[y1, x1] = 0
-                if not card_status.any():
-                    print('Solved in {0} steps'.format(counter))
-                    break
-            else:
-                card_status[y0, x0] = 1
-                card_status[y1, x1] = 1                
-            image = generate_field(card_status, combination)
-            cv2.imshow(window_name, image)
-            opened_cards_number = 0
-        print(card_clicked)
-        card_clicked = 0
+    if clicked_point:
+        y, x = clicked_point
+        
+        if (x % (size_x + pad) > pad) & (y % (size_y + pad) > pad):
+            x = x // (size_x + pad)
+            y = y // (size_y + pad)
+            print(x, y)
+            if card_status[y, x] == 1:
+                card_clicked = (y, x)
+                
+                if opened_cards_number == 0:
+                    opened_cards_number = 1
+                    y0, x0 = card_clicked
+                    card_status[y0, x0] = 2
+                    image = generate_field(card_status, combination)
+                    cv2.imshow(window_name, image)
+                elif opened_cards_number == 1:
+                    opened_cards_number = 2
+                    y1, x1 = card_clicked
+                    card_status[y1, x1] = 2
+                    image = generate_field(card_status, combination)
+                    cv2.imshow(window_name, image)
+                    cv2.waitKey(1000)
+                    counter += 1
+                    if combination[y0, x0] == combination[y1, x1]:
+                        card_status[y0, x0] = 0
+                        card_status[y1, x1] = 0
+                        if not card_status.any():
+                            print('Solved in {0} steps'.format(counter))
+                            break
+                    else:
+                        card_status[y0, x0] = 1
+                        card_status[y1, x1] = 1             
+                    image = generate_field(card_status, combination)
+                    cv2.imshow(window_name, image)
+                    opened_cards_number = 0
+                print(card_clicked)
+        clicked_point = 0
 
 cv2.destroyAllWindows()
 
