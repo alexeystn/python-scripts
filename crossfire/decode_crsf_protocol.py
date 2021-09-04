62567890902f206f0f3bcd20ccb5f8ebf50d3728
Alexey Stankevich <alexey.stn@gmail.com>
2021-09-04 10:41:47 +0300

    Ananlyze CRSF latency

--- crossfire/decode_crsf_protocol.py
+++ crossfire/decode_crsf_protocol.py
@@ -1,44 +1,114 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Log format example:
-# "Async Serial","data",0.0006025,2.375e-05,0xC8
+def load_crsf_rx(filename):
+    
+    # Log format example:
+    # "Async Serial","data",0.0006025,2.375e-05,0xC8
 
-uart_bytes = []
-uart_timestamps = []
+    uart_bytes = []
+    uart_timestamps = []
 
-with open('./capture/decoded_crsf.csv', 'r') as f:
-    f.readline()
-    for line in f:
-        split_line = line.split(sep=',')
-        uart_timestamps.append(int(float(split_line[2])*1e6))
-        uart_bytes.append(int(split_line[4],16))
+    with open(filename, 'r') as f:
+        f.readline()
+        for line in f:
+            split_line = line.split(sep=',')
+            uart_timestamps.append(int(float(split_line[2])*1e6))
+            uart_bytes.append(int(split_line[4],16))
 
-packets = []
-packet_timestamps = []
+    packets = []
+    packet_timestamps = []
 
-for i in range(len(uart_bytes)):
-    if uart_bytes[i] == 200: # 0xC8 = CRSF_SYNC_BYTE
-        length = uart_bytes[i+1]
-        packet = uart_bytes[(i+2):(i+length+2)]
-        packets.append(packet)
-        packet_timestamps.append(uart_timestamps[i+length+1]) # +1 ???
-        i += length
+    for i in range(len(uart_bytes)):
+        if uart_bytes[i] == 200: # 0xC8 = CRSF_SYNC_BYTE
+            length = uart_bytes[i+1]
+            packet = uart_bytes[(i+2):(i+length+2)]
+            packets.append(packet)
+            packet_timestamps.append(uart_timestamps[i+length+1]) # +1 ???
+            i += length
 
-rc_data = []
-rc_timestamps = []
+    rc_data = []
+    rc_timestamps = []
 
-for packet, timestamp in zip(packets, packet_timestamps):
-    if packet[0] == 22: # 0x16 = CRSF_FRAMETYPE_RC_CHANNELS_PACKED
-        packet = packet[1:-1] # remove type and crc
-        packet_bin_8 = ['{0:08b}'.format(i)[::-1] for i in packet] # [::-1] reverse
-        packet_bin_full = ''.join(packet_bin_8)
-        packet_bin_11 = [packet_bin_full[11*i:11*(i+1)] for i in range(16)]
-        rc_packet = [int(b[::-1],2) for b in packet_bin_11]
-        rc_data.append(rc_packet)
-        rc_timestamps.append(timestamp)
+    for packet, timestamp in zip(packets, packet_timestamps):
+        if packet[0] == 22: # 0x16 = CRSF_FRAMETYPE_RC_CHANNELS_PACKED
+            packet = packet[1:-1] # remove type and crc
+            packet_bin_8 = ['{0:08b}'.format(i)[::-1] for i in packet] # [::-1] reverse
+            packet_bin_full = ''.join(packet_bin_8)
+            packet_bin_11 = [packet_bin_full[11*i:11*(i+1)] for i in range(16)]
+            rc_packet = [int(b[::-1],2) for b in packet_bin_11]
+            rc_data.append(rc_packet)
+            rc_timestamps.append(timestamp)
 
-rc_data = np.array(rc_data)
-plt.plot(rc_data)
+    rc_data = np.array(rc_data)
+    return rc_data, rc_timestamps
+
+
+def load_tx_switch(filename, edge):
+
+    # Log format example:
+    # 0.689994000,0
+    # 0.777677250,1
+    
+    with open(filename, 'r') as f:
+        sw_timestamps = []
+        f.readline()
+        for line in f:
+            split_line = line.split(sep=',')
+            if int(split_line[1]) == edge:
+                sw_timestamps.append(int(float(split_line[0])*1e6))
+    return sw_timestamps
+
+
+def find_rx_switches(rc_data, rc_timestamps, threshold):
+    rc_data_diff = np.diff(rc_data)
+    idx = np.where((np.abs(rc_data_diff) > threshold) &
+                   (np.sign(rc_data_diff)==np.sign(threshold)))[0]
+    idx += 1 # next packet is changed
+    return np.array([rc_timestamps[i] for i in idx])
+
+
+sw_time = load_tx_switch('./capture/switch.csv', 0)
+sw_time = sw_time[1:]
+rc_data, rc_time = load_crsf_rx('./capture/crsf_rx.csv')
+rx_time = find_rx_switches(rc_data[:,5], rc_time, 100)
+
+latency = []
+for sw in sw_time:    
+    rx = rx_time[np.where(rx_time > sw)[0].min()]
+    d = rx - sw
+    if d < 50000: # 50 ms switch chatter
+        latency.append(d)
+latency = np.array(latency)/1000
+
+mu = np.mean(latency)
+sigma = np.std(latency)
+
+text = r'$\mu$={0:.1f} ms, $\sigma$={1:.1f} ms'.format(mu,sigma)
+plt.hist(latency, bins=range(3,10), rwidth=0.85)
+plt.grid(axis='y', alpha=1)
+plt.xlabel('Latency, ms')
+plt.ylabel('Counts')
+plt.title('TBS Tracer')
+plt.text(6.5, 18, text)
+plt.ylim((0,25))
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
