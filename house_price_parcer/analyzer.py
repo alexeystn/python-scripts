7e6e8073b5b749bb26b6c1672e9f30ff1acd022c
Alexey Stankevich <alexey.stn@gmail.com>
2022-12-03 17:07:58 +0300

    Add house price parser

---
+++ house_price_parcer/analyzer.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+import sqlite3
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Database:
+
+    filename = 'database_copy.db'
+    timestamp_margin = 300
+
+    def __init__(self):
+        conn = sqlite3.connect(self.filename)
+        self.cursor = conn.cursor()
+
+    def get_timestamps(self):
+        q = """
+        SELECT DISTINCT timestamp
+        FROM flats
+        """
+        m = self.timestamp_margin
+        res = self.cursor.execute(q).fetchall()
+        unique_timestamps = list(set([r[0]//m * m for r in res]))
+        unique_timestamps.sort()
+        datetimes = [datetime.fromtimestamp(t) for t in unique_timestamps]
+        labels = [dt.strftime('%Y-%m-%d %H:%M') for dt in datetimes]
+        return unique_timestamps, labels
+
+    def get_project_names(self):
+        q = """
+        SELECT DISTINCT project
+        FROM flats
+        """
+        res = self.cursor.execute(q).fetchall()
+        return [r[0] for r in res]
+
+
+    def get_number_of_projects_in_sessions(self, timestamps):
+        ret = [0] * len(timestamps)
+        for i, timestamp in enumerate(timestamps):
+            q = """
+            SELECT COUNT(*) 
+            FROM flats 
+            WHERE timestamp BETWEEN {0} AND {1}
+            """.format(timestamp, timestamp + self.timestamp_margin)
+            res = self.cursor.execute(q).fetchall()
+            ret[i] = res[0][0]
+        return  ret
+
+
+    def get_most_exposed_flats(self, project):
+        q = """
+        SELECT flat_id, COUNT(flat_id) 
+        FROM flats 
+        WHERE project='{0}'
+        GROUP BY flat_id
+        ORDER BY COUNT(flat_id) DESC
+        LIMIT 3;
+        """.format(project)
+        res = self.cursor.execute(q).fetchall()
+        return [r[0] for r in res]
+
+
+    def get_price_change(self, flat_id):
+        q = """
+        SELECT price, timestamp/{0}*{0}
+        FROM flats 
+        WHERE flat_id='{1}'
+        """.format(self.timestamp_margin, flat_id)
+        res = self.cursor.execute(q).fetchall()
+        prices = np.array([r[0] for r in res])
+        timestamps = np.array([r[1] for r in res])
+        return prices, timestamps
+        
+            
+
+db = Database()
+timestamps, labels = db.get_timestamps()
+t0 = timestamps[0]
+# print(timestamps)
+
+counts = db.get_number_of_projects_in_sessions(timestamps)
+for label, count in zip(labels, counts):
+    print(label, '-', count)
+
+legend = []
+
+for prj in db.get_project_names():
+    print(prj)
+    flat_ids = db.get_most_exposed_flats(prj)
+    for flat_id in flat_ids:
+        prices, timestamps = db.get_price_change(flat_id)
+        timestamps = (timestamps - t0)/3600/24
+        
+        prices = (prices/prices[0] - 1) * 100
+        legend.append((prj + ' ' + str(flat_id)))
+        plt.plot(timestamps, prices, '-d')
+
+plt.legend(legend)
+plt.show()
+        
+
+
+
+
+
+
+
+
+
---
+++ house_price_parcer/parser.py
@@ -0,0 +1,96 @@
+import re
+import os
+from datetime import datetime
+import sqlite3
+import json
+
+
+class Database:
+
+    filename = 'database.db'
+
+    def __init__(self):
+        if os.path.isfile(self.filename):
+            return
+        conn = sqlite3.connect(self.filename)
+        cursor = conn.cursor()
+        cursor.execute('''
+                  CREATE TABLE IF NOT EXISTS flats
+                  ([record_id] INTEGER PRIMARY KEY,
+                  [project] TEXT,
+                  [flat_id] INTEGER,
+                  [price] INTEGER,
+                  [timestamp] INTEGER
+                  )
+                  ''')
+        conn.commit()
+
+    def write(self, project, id_price_pairs):
+        timestamp = int(datetime.timestamp(datetime.now()))
+        conn = sqlite3.connect(self.filename)
+        cursor = conn.cursor()
+        for id_price in id_price_pairs:
+            cursor.execute('''
+                      INSERT INTO flats (project, flat_id, price, timestamp)
+                      VALUES('{0}',{1},{2},{3});
+                      '''.format(project, id_price[0], id_price[1], timestamp))
+        conn.commit()
+
+
+class Parser:
+
+    filename = 'temp.html'
+    project = ''
+
+    def download_html(self):
+        url = 'https://www.pik.ru/search/{0}'.format(self.project) +\
+              '?rooms=1,2&areaFrom=35&sortBy=price'
+        command = 'curl "{0}" -o {1}'.format(url, self.filename)
+        print(command)
+        os.system(command)
+
+    def get_id_price_pairs(self):
+        with open(self.filename, 'r', encoding="utf-8") as f:
+            for line in f:
+                if len(line) > 1e6:
+                    break
+
+        id_price_pairs = []
+
+        p_price = re.compile('от \d \d{3} \d{3}')
+        p_id = re.compile('data-id="\d{6}"')
+
+        for m_id, m_price in zip(p_id.finditer(line), p_price.finditer(line)):
+            int_id = int(m_id.group()[9:-1])
+            int_price = int(''.join((m_price.group()[3:].split())))
+            id_price_pairs.append((int_id, int_price))
+            print(int_id, int_price)
+
+        return id_price_pairs
+
+    def archive(self):
+        new_filename = './archive/'
+        new_filename += datetime.now().strftime('%Y%m%d_%H%M%S')
+        new_filename += '_' + self.project + '.html'
+        os.rename(self.filename, new_filename)
+
+
+def load_project_list():
+
+    with open('projects.json', 'r') as f:
+        data = json.load(f)
+        print(data)
+        return data
+
+
+db = Database()
+projects = load_project_list()
+
+p = Parser()
+
+for pr in projects:
+    p.project = pr
+    p.download_html()
+    pairs = p.get_id_price_pairs()
+    db.write(pr, pairs)
+    p.archive()
