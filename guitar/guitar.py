6059965d36a2f5316d16c37c22509d53f283c466
Alexey Stankevich <alexey.stn@gmail.com>
2023-12-22 23:59:55 +0300

    Plot guitar fingering

--- guitar/guitar.py
+++ guitar/guitar.py
@@ -4,104 +4,75 @@ import numpy as np
 
 class Box:
 
-    notes = 'ABCDEFG'
+    notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    gamma = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
     height = 120
     width = 200
     radius = 50
     font = cv2.FONT_HERSHEY_DUPLEX
     scale = 2.4
     font_thickness = 6
-    line_thickness = 6  # thickness
+    line_thickness = 4
     margin = 150
 
-    def print_box(self, lines):
+    bg_color_minor = [255, 30, 30]
+    bg_color_major = [0, 0, 230]
+    bg_color_normal = [100, 100, 100]
+    bg_color_unused = [250, 250, 250]
+    txt_color_normal = [255, 255, 255]
+    txt_color_unused = [150, 150, 150]
+
+    def generate_boxes(self, start):
+        result = []
+        notes = self.notes * 4
+        pos = notes.index(self.gamma[start])
+        for i in range(6):
+            start %= 7
+            stop = (start + 2) % 7
+            part = notes[pos:pos+7]
+            start_index = part.index(self.gamma[start])
+            stop_index = part.index(self.gamma[stop])
+            result.append((part, (start_index, stop_index)))
+            pos += 5
+            if i == 3:
+                pos -= 1
+            start += 3
+        return result
+
+    def draw_box(self, box, show=False):
+
+        img = np.ones((self.height*7, self.width*8, 3), dtype='uint8') * 255
 
         for i in range(6):
-            print_line = ['-'] * 7
-            line = lines[6-i-1]
-            for nt in line:
-                print_line[line[nt]] = nt
-            print('|-' + '-|-'.join(print_line) + '-|')
-        print()
-
-    def get_lines(self, start):
-
-        pos = self.notes.index(start)
-        fret_pos = 0
-
-        lines = []
-
-        for i in range(6):
-            lines.append({})
-            for j in range(3):
-                nt = self.notes[pos % 7]
-                lines[i][nt] = fret_pos
-                pos += 1
-                fret_pos += 1 if nt in 'BE' else 2
-            fret_pos -= 4 if i == 3 else 5
-
-        return lines
-
-    def draw_all_boxes(self):
-
-        h = self.height * 7
-        w = self.width * 8
-        m = self.margin
-
-        img = np.ones((5*m+4*h, 3*m+w*2, 3), dtype='uint8')
-
-        for i in range(7):
-            img_box = self.draw_box(self.get_lines(self.notes[i]))
-            x = m + (m + w) * (i % 2)
-            y = m + (m + h) * (i // 2)
-            img[y:y+h, x:x+w, :] = img_box
-
-        cv2.imwrite('img.png', img)
-        return
-
-    def draw_box(self, lines, show=False):
-
-        color_notes = [0, 0, 0]
-        color_white = [255, 255, 255]
-        color_minor = [255, 30, 30]
-        color_major = [0, 0, 230]
-
-        color_background = 255
-
-        img = np.ones((self.height*7, self.width*8, 3), dtype='uint8') * color_background
-
-        for i, line in enumerate(lines):
             y = (6 - i) * self.height
             cv2.line(img, (0, y), (self.width*8, y), [0, 0, 0], self.line_thickness)
+
+            start, stop = box[i][1]
             for j in range(7):
                 x = (j + 1) * self.width
-                cv2.circle(img, (x, y), self.radius, [0, 0, 0], -1, self.line_thickness)
+                txt = box[i][0][j]
+                scale = self.scale
+                font_thickness = self.font_thickness
+                if txt.endswith('#'):
+                    scale /= 2
+                    font_thickness //= 2
+                txt_color = self.txt_color_unused
+                bg_color = self.bg_color_unused
+                if start <= j <= stop:
+                    if not txt.endswith('#'):
+                        txt_color = self.txt_color_normal
+                        bg_color = self.bg_color_normal
+                    if txt == 'A':
+                        bg_color = self.bg_color_minor
+                    elif txt == 'C':
+                        bg_color = self.bg_color_major
 
-            random_note = list(line.keys())[0]
-            note_number = self.notes.find(random_note)
-            note_pos = line[random_note]
-            offset = note_number - note_pos
-            for j in range(7):
-                txt = self.notes[(j + offset) % 7]
-                x = (j + 1) * self.width
-                cv2.putText(img, txt, (x, y),
-                            self.font, self.scale/2, [255, 255, 255], self.font_thickness)
+                cv2.circle(img, (x, y), self.radius, bg_color, -1)
+                centers = cv2.getTextSize(txt, self.font, scale, font_thickness)[0]
 
-            for k in line:
-                color = color_notes
-                if k == 'A':
-                    color = color_minor
-                if k == 'C':
-                    color = color_major
-                x = (line[k] + 1) * self.width
-                cv2.circle(img, (x, y), self.radius, color, -1)
+                cv2.putText(img, txt, (x - centers[0]//2, y + centers[1]//2 - int(self.scale*2)),
+                            self.font, scale, txt_color, font_thickness)
 
-                centers = cv2.getTextSize(k, self.font, self.scale, self.font_thickness)[0]
-                cv2.putText(img, k, (x - centers[0]//2, y + centers[1]//2 - int(self.scale*2)),
-                            self.font, self.scale, [255, 255, 255], self.font_thickness)
-
-            for j in range(7):
-                x = (j + 1) * self.width
                 cv2.circle(img, (x, y), self.radius, [0, 0, 0], self.line_thickness)
 
         for i in range(8):
@@ -112,8 +83,27 @@ class Box:
         if show:
             cv2.imshow('img', img)
             cv2.waitKey()
+            cv2.destroyAllWindows()
 
         return img
+
+    def draw_all_boxes(self):
+
+        h = self.height * 7
+        w = self.width * 8
+        m = self.margin
+
+        img = np.ones((5*m+4*h, 3*m+w*2, 3), dtype='uint8')
+
+        for i in range(7):
+            bx = self.generate_boxes(i)
+            img_bx = self.draw_box(bx)
+            x = m + (m + w) * (i % 2)
+            y = m + (m + h) * (i // 2)
+            img[y:y+h, x:x+w, :] = img_bx
+
+        cv2.imwrite('img.png', img)
+        return
 
 
 b = Box()
