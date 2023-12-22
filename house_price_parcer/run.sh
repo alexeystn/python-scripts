c0ee20989acae443fb69676381d2abfa6298b2d6
Alexey Stankevich <alexey.stn@gmail.com>
2023-12-23 00:10:06 +0300

    Remove unused files

--- house_price_parcer/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/parser
-python3 task_parser.py
-python3 task_analyzer.py
-python3 task_trends.py
-cp database.db ./output/database.db
--- house_price_parcer/server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /root/parser/output
-python3 -m http.server 80
